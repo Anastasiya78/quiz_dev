24c7875b95160b04fb92237237f1b93c2dd754ba
meow <meow@vpn-89-206-113-138.uzh.ch>
2019-01-04 17:51:57 +0100

    new data

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ var quiz = [
                                ],
            "correct" : "So ist es!",
            "explanation" : "Die Aussage entspricht nicht der Realität.",
-           "graphics" : '<img src="img/1.png" alt="Nationalitäten" class="center" height="1000" width="1000">'
+           "graphics" : '<img src="img/1.png" alt="Nationalitäten" class="custom_class" height="500" width=500">'
        },
        {
            "question" : "Mehr Männer als Frauen sind Ausländer",
@@ -23,7 +23,7 @@ var quiz = [
                                ],
            "correct" : "So ist es!",
            "explanation" : "Im Jahr 2017 wohnten in Zürich mehr Männer mit einem ausländischen Pass",
-           "graphics" : '<img src="img/2.png" alt="Geschlecht" class="center" height="200" width="200">'
+           "graphics" : '<img src="img/2.png" alt="Geschlecht" class="custom_class" height="500" width="500">'
        },
        {
            "question" : "Der Ausländeranteil ist im Oerlikon am grössten.",
@@ -34,7 +34,7 @@ var quiz = [
                                ],
            "correct" : "Stimmt nicht!",
            "explanation" : "In der Mitte",
-           "graphics" : '<img src="img/3.png" alt="Stadtquartier" class="center">'
+           "graphics" : '<img src="img/3.png" alt="Stadtquartier" class="custom_class" height="500" width="500">'
        },
        {
            "question" : "Die Mehrheit der <18 Jährigen in Zürich sind Ausländer",
@@ -45,7 +45,7 @@ var quiz = [
                                ],
            "correct" : "Stimmt nicht!",
            "explanation" : "Fast",
-           "graphics" : '<img src="img/4.png" alt="Alter" class="center">'
+           "graphics" : '<img src="img/4.png" alt="Alter" class="custom_class" height="500" width="500">'
        },
        {
            "question" : "Mehr als die Hälfte der Ausländer sind ledig.",
@@ -56,7 +56,7 @@ var quiz = [
                                ],
            "correct" : "So ist es!",
            "explanation" : "In der Mitte",
-           "graphics" : '<img src="img/5.png" alt="Zivilstand" class="center">'
+           "graphics" : '<img src="img/5.png" alt="Zivilstand" class="custom_class" height="500" width="500">'
        },
        
        {
@@ -68,7 +68,7 @@ var quiz = [
                                ],
            "correct" : "Stimmt nicht!",
            "explanation" : "Im Jahr 2017 wohnten in Zürich mehr Männer mit einem ausländischen Pass",
-           "graphics" : '<img src="img/6.png" alt="Kinder" class="center">'
+           "graphics" : '<img src="img/6.png" alt="Kinder" class="custom_class" height="500" width="500">'
        },
 
    ];
